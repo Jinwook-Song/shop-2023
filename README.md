40d9408fab99592499340b85d2f5840e8cd70485
jinwook song <wlsdnr129@naver.com>
2023-06-21 00:43:03 +0900

    feat: upload image to cloudinary

--- README.md
+++ README.md
@@ -212,3 +212,23 @@ export default function ProtectedRoute({
   return <>{children}</>;
 }
 ```
+
+## File Upload (cloudinary)
+
+[docs](https://cloudinary.com/documentation/upload_images#code_explorer_upload_multiple_files_using_a_form_unsigned)
+
+api > upload.ts
+
+```tsx
+export async function uploadImage(file: File) {
+  const data = new FormData();
+  data.append('file', file);
+  data.append('upload_preset', process.env.REACT_APP_CLOUDINARY_PRESET!);
+  return fetch(process.env.REACT_APP_CLOUDINARY_URL!, {
+    method: 'POST',
+    body: data,
+  })
+    .then((res) => res.json())
+    .then((data) => data.url);
+}
+```
--- src/pages/NewProduct.tsx
+++ src/pages/NewProduct.tsx
@@ -1,5 +1,87 @@
-import React from 'react';
+import { uploadImage } from 'api/uploader';
+import Button from 'components/ui/Button';
+import { ChangeEvent, FormEvent, useState } from 'react';
+
+type ProductInputType = {
+  title: string;
+  price: string;
+  category: string;
+  description: string;
+  options: string;
+  [key: string]: any;
+};
 
 export default function NewProduct() {
-  return <div>NewProduct</div>;
+  const [product, setProduct] = useState<ProductInputType>();
+  const [file, setFile] = useState<File>();
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    uploadImage(file!).then((url) => console.log(url));
+  };
+
+  const handleChnage = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value, files } = e.currentTarget;
+    if (name === 'file' && files) {
+      setFile(files[0]);
+      return;
+    }
+
+    setProduct((product) => ({ ...product!, [name]: value }));
+  };
+  return (
+    <section>
+      {file && <img src={URL.createObjectURL(file)} alt={file.name} />}
+      <form onSubmit={handleSubmit}>
+        <input
+          type='file'
+          name='file'
+          accept='image/*'
+          required
+          onChange={handleChnage}
+        />
+        <input
+          type='text'
+          name='title'
+          value={product?.title ?? ''}
+          placeholder='제품명'
+          required
+          onChange={handleChnage}
+        />
+        <input
+          type='number'
+          name='price'
+          value={product?.price ?? ''}
+          placeholder='가격'
+          required
+          onChange={handleChnage}
+        />
+        <input
+          type='text'
+          name='category'
+          value={product?.category ?? ''}
+          placeholder='카테고리'
+          required
+          onChange={handleChnage}
+        />
+        <input
+          type='text'
+          name='description'
+          value={product?.description ?? ''}
+          placeholder='제품 설명'
+          required
+          onChange={handleChnage}
+        />
+        <input
+          type='text'
+          name='options'
+          value={product?.options ?? ''}
+          placeholder='옵션(콤마로 구분)'
+          required
+          onChange={handleChnage}
+        />
+        <Button text={'제품 등록하기'} onClick={() => {}} />
+      </form>
+    </section>
+  );
 }
